109ccbddeb0339b4cbd523ef5f135d9036cd8935
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2025-03-07 00:24:51 +0900

    Update apply.constant.ts

--- packages/constants/apply.constant.ts
+++ packages/constants/apply.constant.ts
@@ -111,7 +111,7 @@ export const TAG_OPTIONS = [
   },
 ];
 
-export const REVIEW_START_DATE = '2025-03-07T00:00:00';
+export const REVIEW_START_DATE = '2025-03-06T15:00:00';
 export const REVIEW_END_DATE = '2025-03-08T10:00:00';
 
 export const MENTOR_RECRUIT_START_DATE = '2025-02-10';
