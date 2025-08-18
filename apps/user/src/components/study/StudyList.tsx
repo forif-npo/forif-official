5ba8eb104fa9196e111e7c2f19690d88a3cf6d29
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2025-08-18 16:15:12 +0900

    hotfix: add alternative study image

--- apps/user/src/components/study/StudyList.tsx
+++ apps/user/src/components/study/StudyList.tsx
@@ -56,22 +56,11 @@ export function StudyList({ year, semester, difficulty }: StudyProps) {
   return (
     <Box sx={{ px: { xs: 4, md: 8, xl: 12 }, pb: 4, margin: 'auto' }}>
       <Grid container spacing={{ xs: 2, xl: 4 }}>
-        <Grid item xl={3} md={4} sm={6} xs={12}>
-          <StudyCard
-            id={0}
-            image={'/cutie.svg'}
-            primaryMentorName={''}
-            secondaryMentorName={''}
-            difficulty={0}
-            title={'자율스터디'}
-            tag='자율'
-          />
-        </Grid>
         {studies.map((study) => (
           <Grid key={study.id} item xl={3} md={4} sm={6} xs={12}>
             <StudyCard
               id={study.id}
-              image={study.image!}
+              image={'/cutie.svg'}
               primaryMentorName={study.primary_mentor_name}
               secondaryMentorName={study.secondary_mentor_name}
               difficulty={study.difficulty}
@@ -91,6 +80,17 @@ export function StudyList({ year, semester, difficulty }: StudyProps) {
             </Typography>
           </CenteredBox>
         )}
+        <Grid item xl={3} md={4} sm={6} xs={12}>
+          <StudyCard
+            id={0}
+            image={'/cutie.svg'}
+            primaryMentorName={''}
+            secondaryMentorName={''}
+            difficulty={0}
+            title={'자율스터디'}
+            tag='자율'
+          />
+        </Grid>
       </Grid>
     </Box>
   );
