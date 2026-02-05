ba4a4effc9a645175e48c9a4a98ccfffde0fc7bb
yunsu-shin <yunsu102896@gmail.com>
2026-02-05 16:37:09 +0900

    Update apply.constant.ts

--- packages/constants/apply.constant.ts
+++ packages/constants/apply.constant.ts
@@ -117,8 +117,8 @@ export const REVIEW_END_DATE = '2026-02-18T23:59:59';
 export const MENTOR_RECRUIT_START_DATE = '2026-02-09';
 export const MENTOR_RECRUIT_END_DATE = '2026-02-15';
 
-export const RECRUIT_START_DATE = '2026-00-00';
-export const RECRUIT_END_DATE = '2026-00-00T15:00:00';
+export const RECRUIT_START_DATE = '2026-02-23';
+export const RECRUIT_END_DATE = '2026-03-05T15:00:00';
 
 export const CURRENT_YEAR = 2026;
 export const CURRENT_SEMESTER = 1;
